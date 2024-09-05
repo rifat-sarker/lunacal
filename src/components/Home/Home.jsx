dfb70cdc782cb430877990999b7b403746da5461
Rifat Sarker <rifatswd@gmail.com>
2024-09-05 13:00:08 +0600

    fixed styles,make responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -43,10 +43,10 @@ const Home = () => {
   );
 
   return (
-    <div>
+    <div className="p-4">
       <div className="flex">
         <div className="w-1/2"></div>
-        <div className="bg-[#363C43] text-[#969696] rounded-3xl relative w-[720px] h-[316px]">
+        <div className="bg-[#363C43] text-[#969696] rounded-3xl relative w-[750px] ">
           <div className="flex flex-col gap-4 justify-between absolute top-5 mx-2 h-1/2 ">
             <FaRegQuestionCircle size={25} />
             <HiMiniSquares2X2 size={25} />
@@ -64,38 +64,40 @@ const Home = () => {
                 Recommended
               </Tab>
             </TabList>
-            <TabPanels className="font-jakarta-sans">
+            <TabPanels className="font-jakarta-sans text-[20px]">
               <TabPanel>
                 <p>
                   Hello! I’m Dave, your sales rep here from Salesforce. I’ve
-                  been working at this awesome company for 3 years now. I was
-                  born and raised in Albany, NY& have been living in Santa Carla
-                  for the past 10 years my wife Tiffany and my 4 year old twin
-                  daughters- Emma and Ella. Both of them are just starting
-                  school, so my calender is usually blocked between 9-10 AM.
-                  This is a...
+                  been working at this awesome company for 3 years now. <br />{" "}
+                  <br /> I was born and raised in Albany, NY& have been living
+                  in Santa Carla for the past 10 years my wife Tiffany and my 4
+                  year old twin daughters- Emma and Ella. Both of them are just
+                  starting school, so my calender is usually blocked between
+                  9-10 AM. This is a...
                 </p>
               </TabPanel>
               <TabPanel>
                 <p>
-                  Hello! dI’m Dave, your sales rep here from Salesforce. I’ve
-                  been working at this awesome company for 3 years now. I was
-                  born and raised in Albany, NY& have been living in Santa Carla
-                  for the past 10 years my wife Tiffany and my 4 year old twin
-                  daughters- Emma and Ella. Both of them are just starting
-                  school, so my calender is usually blocked between 9-10 AM.
-                  This is a...
+                  I’ve been with Salesforce for 3 years, and during that time,
+                  I’ve had the privilege of working with a wide range of clients
+                  across various industries. My focus has always been on
+                  understanding each client’s unique needs and helping them
+                  leverage Salesforce to its fullest potential. From
+                  streamlining sales processes to improving customer service,
+                  I’ve been able to guide businesses in finding the right
+                  solutions that drive success.
                 </p>
               </TabPanel>
               <TabPanel>
                 <p>
-                  Hello! I’m Dave, your sales rep here from Salesforce. I’ve
-                  been working at this awesome company for 3 years now. I was
-                  born and raised in Albany, NY& have been living in Santa Carla
-                  for the past 10 years my wife Tiffany and my 4 year old twin
-                  daughters- Emma and Ella. Both of them are just starting
-                  school, so my calender is usually blocked between 9-10 AM.
-                  This is a...
+                  Based on my 3 years at Salesforce, I recommend some great
+                  solutions that might be perfect for your business. Salesforce
+                  CRM is ideal for managing customer relationships and
+                  streamlining your sales processes. You can also benefit from
+                  Marketing Cloud, which automates your marketing efforts across
+                  email, social media, and more. For top-notch customer service,
+                  I’d suggest Service Cloud to help you track support tickets
+                  and customer inquiries.
                 </p>
               </TabPanel>
             </TabPanels>
@@ -104,15 +106,20 @@ const Home = () => {
       </div>
       <div className="flex my-8">
         <div className="w-1/2"></div>
-        <div className="bg-[#363C43] text-[#969696] rounded-3xl relative w-[720px] h-[316px]">
+        <div className="bg-[#363C43] text-[#969696] rounded-3xl relative w-[750px]">
           <div className="flex flex-col gap-4 justify-between absolute top-5 mx-2 h-1/2 ">
             <FaRegQuestionCircle size={25} />
             <HiMiniSquares2X2 size={25} />
           </div>
 
-          <div className="flex justify-around items-center px-8 py-4">
-            <button className="font-poppins text-white bg-[#171717] px-6 py-3 rounded-2xl">Gallery</button>
-            <label htmlFor="fileInput" className="add-image-button bg-gradient-to-r shadow-[#6F787C] bg-opacity-20 font-bold from-[#303439] to-[#161718] rounded-3xl shadow-2xl ">
+          <div className="flex justify-between items-center gap-4 px-10 py-4">
+            <button className="font-poppins text-white bg-[#171717] px-6 py-3 rounded-2xl">
+              Gallery
+            </button>
+            <label
+              htmlFor="fileInput"
+              className="add-image-button py-3 font-jakarta-sans text-[12px]  bg-gradient-to-tr shadow-2xl from-[#FFFFFF26] to-[#FFFFFF0D] font-bold rounded-3xl"
+            >
               + ADD IMAGE
             </label>
             <input
@@ -123,22 +130,30 @@ const Home = () => {
               style={{ display: "none" }}
             />
             <div className="flex gap-4">
-              <button className="custom-left-nav border bg-gradient-to-tr from-[#303439] to-[#161718] rounded-full p-2 text-[#6F787C]" onClick={prevPage} disabled={currentPage === 0}>
-              <FaArrowLeft size={20} />
+              <button
+                className="custom-left-nav border bg-gradient-to-tr from-[#303439] to-[#161718] rounded-full p-2 text-[#6F787C] shadow-2xl"
+                onClick={prevPage}
+                disabled={currentPage === 0}
+              >
+                <FaArrowLeft size={20} />
               </button>
-              <button className="custom-right-nav border bg-gradient-to-tr from-[#303439] to-[#161718] rounded-full p-2 text-[#6F787C]" onClick={nextPage} disabled={currentPage >= totalPages - 1}>
-              <FaArrowRight size={20} />
+              <button
+                className="custom-right-nav border bg-gradient-to-tr from-[#303439] to-[#161718] rounded-full p-2 text-[#6F787C]  shadow-2xl"
+                onClick={nextPage}
+                disabled={currentPage >= totalPages - 1}
+              >
+                <FaArrowRight size={20} />
               </button>
             </div>
           </div>
 
-          <div className="gallery-container">
+          <div className="gallery-container lg:flex flex-wrap gap-4 justify-center justify-items-center ">
             {currentImages.map((img, index) => (
               <img
                 key={index}
                 src={img.original}
                 alt={`Gallery image ${index}`}
-                className="gallery-image"
+                className="gallery-image my-2"
               />
             ))}
           </div>
