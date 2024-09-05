70bae4990a22be41cf3d31dea33cc92d74d56d82
Rifat Sarker <rifatswd@gmail.com>
2024-09-05 12:06:55 +0600

    part 2 done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,13 @@ import image from "../../assets/Rectangle 39335.png";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { useState } from "react";
+import "./Gallery.css";
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Home = () => {
   const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const handleAddImage = (event) => {
     const file = event.target.files[0];
@@ -17,6 +21,26 @@ const Home = () => {
       setImages([...images, { original: imageUrl, thumbnail: imageUrl }]);
     }
   };
+
+  const imagesPerPage = 3;
+  const totalPages = Math.ceil(images.length / imagesPerPage);
+
+  const nextPage = () => {
+    if (currentPage < totalPages - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const currentImages = images.slice(
+    currentPage * imagesPerPage,
+    currentPage * imagesPerPage + imagesPerPage
+  );
 
   return (
     <div>
@@ -85,12 +109,38 @@ const Home = () => {
             <FaRegQuestionCircle size={25} />
             <HiMiniSquares2X2 size={25} />
           </div>
-          <div className="absolute right-3 top-20">
-            <img src={image} alt="" />
+
+          <div className="flex justify-around items-center px-8 py-4">
+            <button className="font-poppins text-white bg-[#171717] px-6 py-3 rounded-2xl">Gallery</button>
+            <label htmlFor="fileInput" className="add-image-button bg-gradient-to-r shadow-[#6F787C] bg-opacity-20 font-bold from-[#303439] to-[#161718] rounded-3xl shadow-2xl ">
+              + ADD IMAGE
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              accept="image/*"
+              onChange={handleAddImage}
+              style={{ display: "none" }}
+            />
+            <div className="flex gap-4">
+              <button className="custom-left-nav border bg-gradient-to-tr from-[#303439] to-[#161718] rounded-full p-2 text-[#6F787C]" onClick={prevPage} disabled={currentPage === 0}>
+              <FaArrowLeft size={20} />
+              </button>
+              <button className="custom-right-nav border bg-gradient-to-tr from-[#303439] to-[#161718] rounded-full p-2 text-[#6F787C]" onClick={nextPage} disabled={currentPage >= totalPages - 1}>
+              <FaArrowRight size={20} />
+              </button>
+            </div>
           </div>
-          <div>
-            <input type="file" accept="image/*" onChange={handleAddImage} />
-            {images.length > 0 && <ImageGallery items={images} />}
+
+          <div className="gallery-container">
+            {currentImages.map((img, index) => (
+              <img
+                key={index}
+                src={img.original}
+                alt={`Gallery image ${index}`}
+                className="gallery-image"
+              />
+            ))}
           </div>
         </div>
       </div>
